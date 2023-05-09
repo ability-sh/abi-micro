21ea228ff9697661a77e9dfc254ff99c56abf1f5
T-rex <0x50fc@gmail.com>
2023-05-09 10:49:18 +0800

    add doc scheme

--- micro/Executor.go
+++ micro/Executor.go
@@ -1,5 +1,28 @@
 package micro
 
 type Executor interface {
+	Scheme() *Scheme
 	Exec(ctx Context, name string, data interface{}) (interface{}, error)
+}
+
+type SchemeObject struct {
+	Fields []*SchemeField `json:"fields"`
+}
+
+type SchemeField struct {
+	Type     string `json:"type"`
+	Name     string `json:"name"`
+	Required bool   `json:"required"`
+}
+
+type SchemeItem struct {
+	Name   string        `json:"name"`
+	Task   *SchemeObject `json:"task"`
+	Result *SchemeObject `json:"result"`
+}
+
+type Scheme struct {
+	Name    string                   `json:"name"`
+	Items   []*SchemeItem            `json:"items"`
+	Objects map[string]*SchemeObject `json:"objects"`
 }
--- runtime/reflect.go
+++ runtime/reflect.go
@@ -12,8 +12,9 @@ import (
 )
 
 type reflectExecutor struct {
-	s    interface{}
-	exec map[string]reflect.Value
+	s      interface{}
+	exec   map[string]reflect.Value
+	scheme *micro.Scheme
 }
 
 var contextType = reflect.TypeOf((*micro.Context)(nil)).Elem()
@@ -35,13 +36,111 @@ func getName(name string, b *bytes.Buffer) string {
 	return b.String()
 }
 
+func eachField(tp reflect.Type, fn func(name string, required bool, t reflect.StructField) bool) {
+	count := tp.NumField()
+
+	for i := 0; i < count; i++ {
+
+		tf := tp.Field(i)
+
+		name := tf.Tag.Get("name")
+
+		if name == "" {
+			name = tf.Tag.Get("json")
+		}
+
+		if name == "-" {
+			continue
+		}
+
+		if tf.Type.Kind() == reflect.Struct {
+			eachField(tf.Type, fn)
+			continue
+		}
+
+		if name == "" {
+			continue
+		}
+
+		ns := strings.Split(name, ",")
+		name = ns[0]
+		required := true
+
+		if len(ns) > 1 && ns[1] == "omitempty" {
+			required = false
+		}
+
+		if !fn(name, required, tf) {
+			break
+		}
+
+	}
+}
+
+func typeToSchemeType(t reflect.Type, scheme *micro.Scheme) string {
+	switch t.Kind() {
+	case reflect.String:
+		return "string"
+	case reflect.Bool:
+		return "bool"
+	case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32:
+		return "int32"
+	case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32:
+		return "uint32"
+	case reflect.Int64:
+		return "int64"
+	case reflect.Uint64:
+		return "uint64"
+	case reflect.Float32:
+		return "float"
+	case reflect.Float64:
+		return "double"
+	case reflect.Slice:
+		return typeToSchemeType(t.Elem(), scheme) + "[]"
+	case reflect.Ptr:
+		return typeToSchemeType(t.Elem(), scheme)
+	case reflect.Struct:
+		_, ok := scheme.Objects[t.Name()]
+		if !ok {
+			dst := &micro.SchemeObject{}
+			typeToSchemeObject(t, dst, scheme)
+			scheme.Objects[t.Name()] = dst
+		}
+		return t.Name()
+	}
+	return "any"
+}
+
+func typeFieldToSchemeItem(name string, required bool, tf reflect.StructField, scheme *micro.Scheme) *micro.SchemeField {
+	rs := micro.SchemeField{Name: name, Required: required}
+	rs.Type = typeToSchemeType(tf.Type, scheme)
+	return &rs
+}
+
+func typeToSchemeObject(t reflect.Type, dst *micro.SchemeObject, scheme *micro.Scheme) {
+
+	switch t.Kind() {
+	case reflect.Ptr:
+		tp := t.Elem()
+		if tp.Kind() == reflect.Struct {
+			eachField(tp, func(name string, required bool, tf reflect.StructField) bool {
+				dst.Fields = append(dst.Fields, typeFieldToSchemeItem(name, required, tf, scheme))
+				return true
+			})
+		}
+	}
+
+}
+
 func NewReflectExecutor(s interface{}) micro.Executor {
-	rs := &reflectExecutor{s: s, exec: map[string]reflect.Value{}}
+	rs := &reflectExecutor{s: s, exec: map[string]reflect.Value{}, scheme: &micro.Scheme{Objects: map[string]*micro.SchemeObject{}}}
 
 	v := reflect.ValueOf(s)
 	t := v.Type()
 
 	num := v.NumMethod()
+
+	rs.scheme.Name = t.Name()
 
 	b := bytes.NewBuffer(nil)
 
@@ -81,6 +180,13 @@ func NewReflectExecutor(s interface{}) micro.Executor {
 
 		rs.exec[n] = m
 
+		schemeItem := micro.SchemeItem{Name: n, Task: &micro.SchemeObject{}, Result: &micro.SchemeObject{}}
+
+		typeToSchemeObject(inType, schemeItem.Task, rs.scheme)
+		typeToSchemeObject(m.Type().Out(0), schemeItem.Result, rs.scheme)
+
+		rs.scheme.Items = append(rs.scheme.Items, &schemeItem)
+
 		log.Println("Executor", "=>", n, "=>", name)
 
 	}
@@ -116,4 +222,8 @@ func (r *reflectExecutor) Exec(ctx micro.Context, name string, data interface{})
 	} else {
 		return nil, errors.Errorf(404, "Not Found %s", name)
 	}
+}
+
+func (r *reflectExecutor) Scheme() *micro.Scheme {
+	return r.scheme
 }
